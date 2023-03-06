ffe2afc30ec00679cb4ce8dd648799bdeca18918
jscsonka <113357554+jscsonka@users.noreply.github.com>
2023-03-06 16:12:19 -0500

    Saving work

--- djangodemo/myapp/models.py
+++ djangodemo/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateTimeField('Movie Release Date')
+    movei_data = models.CharField(max_length=1000)
+
 # Create your models here.
